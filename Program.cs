a568eac1306ecc2373bd10015ed09536f994a67c
Jasunltd <63054751+Jasunltd@users.noreply.github.com>
2022-04-22 01:23:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ * 	   File name: Temperature Table.cs
+ * 	Project name: Homework 6
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 Jameel Salami salamij@etsu.edu				
+ *          Course-Section:  002
+ *           Creation Date:	 30 March 2022		
+ * -------------------------------------------------------------------
+ */
+
+public class Program
+{
+    public static double celsius(double f)
+    {
+        var c = 5.0 * (f - 32.0) / 9;
+        return c;
+    }
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Fahrenheit     Celcius");
+        for (int f = 0; f <= 20; f++)
+        {
+            Console.Write(f + "              " + celsius(f) + "\n" );
+        }
+    }
+}
